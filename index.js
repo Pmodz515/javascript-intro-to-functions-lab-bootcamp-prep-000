25db7ec757368779e7548a2d0885c2d374c09d04
Patrick Modafferi <pat.modafferi@gmail.com>
2017-09-29 17:23:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function logShout(string){
 function logWhisper(string){
   console.log('hello');string.toLowerCase()
 }
+function sayHiToGrandma(string){
+return string("I can't hear you!")
+if string.toLowerCase(hello) {
+
+}
