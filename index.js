a8e1eeae423a2c2ec577b6e67ae4122f55528ae8
Patrick Modafferi <pat.modafferi@gmail.com>
2017-09-29 17:04:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string){
-  console.log('hello');string.toUpperCase('HELLO')
+  console.log('HELLO');string.toUpperCase()
 }
